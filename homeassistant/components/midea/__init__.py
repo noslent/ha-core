fe9ad5291e92cced5ea0175059efb21dab1d3baf
noslent <234648082+noslent@users.noreply.github.com>
2026-08-08 01:01:15 -0300

    Add generated Midea English translations

--- homeassistant/components/midea/__init__.py
+++ homeassistant/components/midea/__init__.py
@@ -20,7 +20,11 @@ from homeassistant.exceptions import ConfigEntryError, ConfigEntryNotReady
 from .const import CONF_KEY, CONF_SUBTYPE
 from .entity import MideaConfigEntry
 
-_PLATFORMS: list[Platform] = [Platform.CLIMATE, Platform.SENSOR]
+_PLATFORMS: list[Platform] = [
+    Platform.BINARY_SENSOR,
+    Platform.CLIMATE,
+    Platform.SENSOR,
+]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: MideaConfigEntry) -> bool:
